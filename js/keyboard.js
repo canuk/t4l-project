dd5e9985663bf8027d84c4f4e85d8d06dc5606a5
Reuben Thiessen <reubenthiessen@gmail.com>
2015-11-17 22:55:46 -0800

    initial commit

---
+++ js/keyboard.js
@@ -0,0 +1,81 @@
+$(function(){
+    var $write = $('#write'),
+        shift = false,
+        capslock = false;
+     
+    $('#keyboard li').click(function(){
+        var $this = $(this),
+            character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
+        // Shift keys
+        if ($this.hasClass('left-shift') || $this.hasClass('right-shift')) {
+            $('.letter').toggleClass('uppercase');
+            $('.symbol span').toggle();
+             
+            shift = (shift === true) ? false : true;
+            capslock = false;
+            return false;
+        }
+         
+        // Caps lock
+        if ($this.hasClass('capslock')) {
+            $('.letter').toggleClass('uppercase');
+            $('.capslock').toggleClass('capslock-shade');
+            capslock = true;
+            return false;
+        }
+         
+        // Delete
+        if ($this.hasClass('delete')) {
+            var html = $write.html();
+             
+            $write.html(html.substr(0, html.length - 1));
+            return false;
+        }
+         
+        // Special characters
+        if ($this.hasClass('symbol')) character = $('span:visible', $this).html();
+        if ($this.hasClass('space')) character = ' ';
+        if ($this.hasClass('tab')) character = "\t";
+        if ($this.hasClass('return')) character = "\n";
+         
+        // Uppercase letter
+        if ($this.hasClass('uppercase')) character = character.toUpperCase();
+         
+        // Remove shift once a key is clicked.
+        if (shift === true) {
+            $('.symbol span').toggle();
+            if (capslock === false) {
+               $('.letter').toggleClass('uppercase');
+            }; 
+            shift = false;
+        }
+
+        // Add the character
+        $('#write').val($('#write').val() + character);       
+
+        $write.focus();
+        moveCursorToEnd('textarea');        
+        $write.on('focus', function () {
+            var element = this;
+            // Cursor has not set yet. And wait 100ms to skip global click event.
+            setTimeout(function () {
+                // Cursor is ready.
+                $write.textcomplete('trigger');
+            }, 100);
+        });
+        
+        
+    });
+});
+
+
+function moveCursorToEnd(el) {
+    if (typeof el.selectionStart == "number") {
+        el.selectionStart = el.selectionEnd = el.value.length;
+    } else if (typeof el.createTextRange != "undefined") {
+        el.focus();
+        var range = el.createTextRange();
+        range.collapse(false);
+        range.select();
+    }
+}
